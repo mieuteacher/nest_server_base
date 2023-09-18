7b61f220f5016074c48306ee5d17437fe2476bf1
Mieu Teacher <mieuteacher@gmail.com>
2023-09-18 09:17:16 +0700

    typeorm env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,17 @@ import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), // load env toàn hệ thống
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'server_db',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DBNAME,
       entities: ["dist/**/*.entity{.ts,.js}"],
       synchronize: true, // dev
     }),
-    ConfigModule.forRoot(),
     CategoriesModule
   ],
   controllers: [AppController],
